bd5c52e5319c3016573cbf3d8450f471d0ed582f
destinytaoer <1848765519@qq.com>
2019-06-11 21:49:45 +0800

    完成 Vue 单文件目录以及按钮样式，并启动 parcel

---
+++ src/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="g-button">按钮</button>    
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+  .g-button {
+    height: var(--btn-height);
+    padding: 0 1em;
+    border-radius: var(--border-raduis);
+    border: 1px solid var(--border-color);
+    font-size: var(--font-size);
+    color: var(--color);
+    background: var(--btn-bg);
+    &:hover {
+      background: var(--btn-hover-bg);
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background: var(--btn-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Button from './Button'
+
+Vue.component('g-button', Button)
+
+new Vue({
+  el: '#app'
+})
