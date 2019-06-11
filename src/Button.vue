ba992e58f3d379d9d7c50c03eb0d823a41848f42
destinytaoer <1848765519@qq.com>
2019-06-11 23:19:30 +0800

    拆分 icon 组件

--- src/Button.vue
+++ src/Button.vue
@@ -4,14 +4,9 @@
     class="g-button"
     :class="{'icon-right': iconPos === 'right'}"
   >
-    <svg
-      v-if="icon"
-      class="icon"
-    >
-      <use :xlink:href="`#i-${icon}`"></use>
-    </svg>
+    <g-icon v-if="icon" :name="icon"></g-icon>
     <div class="content">
-      <slot></slot>
+      <slot/>
     </div>
   </button>
 </template>
---
+++ src/Icon.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg class="icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+<script>
+export default {
+  props: ["name"]
+};
+</script>
+<style lang="scss">
+</style>
