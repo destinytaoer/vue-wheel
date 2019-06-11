4a1d7a7395424dc2c55ecbccd74f05395928e5fe
destinytaoer <1848765519@qq.com>
2019-06-11 23:08:50 +0800

    给 button 组件增加 icon 和 iconPos 属性

--- src/Button.vue
+++ src/Button.vue
@@ -1,29 +1,76 @@
 <template>
-  <button class="g-button">按钮</button>    
+  <!-- 利用 class 决定 icon 位置 利用 flex 的 order-->
+  <button
+    class="g-button"
+    :class="{'icon-right': iconPos === 'right'}"
+  >
+    <svg
+      v-if="icon"
+      class="icon"
+    >
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
 export default {
-  
-}
-</script>
-<style lang="scss">
-  .g-button {
-    height: var(--btn-height);
-    padding: 0 1em;
-    border-radius: var(--border-raduis);
-    border: 1px solid var(--border-color);
-    font-size: var(--font-size);
-    color: var(--color);
-    background: var(--btn-bg);
-    &:hover {
-      background: var(--btn-hover-bg);
-      border-color: var(--border-color-hover);
-    }
-    &:active {
-      background: var(--btn-active-bg);
-    }
-    &:focus {
-      outline: none;
+  props: {
+    icon: {
+      type: String
+    },
+    iconPos: {
+      type: String,
+      default: 'left',
+      validator(value) {
+         // 校验属性 left right
+        return value === 'left' || value === 'right'
+      }
     }
   }
+};
+</script>
+<style lang="scss">
+.g-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  height: var(--btn-height);
+  padding: 0 1em;
+  border-radius: var(--border-raduis);
+  border: 1px solid var(--border-color);
+  font-size: var(--font-size);
+  color: var(--color);
+  background: var(--btn-bg);
+  vertical-align: middle;
+  &:hover {
+    background: var(--btn-hover-bg);
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background: var(--btn-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: .3em;
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .3em;
+    }
+    > .content {
+      order: 1;
+    }
+  }
+}
 </style>
