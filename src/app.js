8b5c42565fb1be63c50a4e3c9a5488fe4bc6bc91
destinytaoer <1848765519@qq.com>
2019-06-12 10:57:48 +0800

    引入 chai-spies 监听函数的执行，来完成 click 测试用例

--- src/app.js
+++ src/app.js
@@ -17,85 +17,97 @@ new Vue({
 })
 
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
 const expect = chai.expect
 // 单元测试，编写测试用例
-{
-  // 测试 icon 路径
-  const Constructor = Vue.extend(Button)
-  let vm = new Constructor({
-    propsData: {
-      icon: 'settings'
-    }
-  }).$mount()
-  let useEl = vm.$el.querySelector('use')
-  let href = useEl.getAttribute('xlink:href')
-  expect(href).to.equal('#i-settings')
-  vm.$destroy()
-}
-{
-  // 测试 loading 时的 icon 路径
-  const Constructor = Vue.extend(Button)
-  let vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-      loading: true
-    }
-  }).$mount()
-  let useEl = vm.$el.querySelector('use')
-  let href = useEl.getAttribute('xlink:href')
-  expect(href).to.equal('#i-loading')
-  vm.$destroy()
-}
-{
-  // 测试 icon 的 order 属性
-  // 注意元素出现在页面中的才会具有 CSS 属性
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  let vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-      loading: true
-    }
-  }).$mount(div)
-  let svg = vm.$el.querySelector('svg')
-  let { order } = window.getComputedStyle(svg)
-  expect(order).to.eq("1")
-  vm.$el.remove()
-  vm.$destroy()
-}
-{
-  // 测试 icon 的 order 属性
-  // 注意元素出现在页面中的才会具有 CSS 属性
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  let button = new Constructor({
-    propsData: {
-      icon: 'settings',
-      iconPos: 'right',
-      loading: true
-    }
-  }).$mount(div)
-  let svg = button.$el.querySelector('svg')
-  let { order } = window.getComputedStyle(svg)
-  expect(order).to.eq("2")
-  button.$el.remove()
-  button.$destroy()
-}
-{
-  // 测试 click 事件
-  const Constructor = Vue.extend(Button)
-  let vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-      iconPos: 'right',
-      loading: true
-    }
-  }).$mount()
-  vm.$on('click', function () {
-    console.log(1)
+try {
+  {
+    // 测试 icon 路径
+    const Constructor = Vue.extend(Button)
+    let vm = new Constructor({
+      propsData: {
+        icon: 'settings'
+      }
+    }).$mount()
+    let useEl = vm.$el.querySelector('use')
+    let href = useEl.getAttribute('xlink:href')
+    expect(href).to.equal('#i-settings')
+    vm.$destroy()
+  }
+  {
+    // 测试 loading 时的 icon 路径
+    const Constructor = Vue.extend(Button)
+    let vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        loading: true
+      }
+    }).$mount()
+    let useEl = vm.$el.querySelector('use')
+    let href = useEl.getAttribute('xlink:href')
+    expect(href).to.equal('#i-loading')
+    vm.$destroy()
+  }
+  {
+    // 测试 icon 的 order 属性
+    // 注意元素出现在页面中的才会具有 CSS 属性
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    let vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        loading: true
+      }
+    }).$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq("1")
+    vm.$el.remove()
+    vm.$destroy()
+  }
+  {
+    // 测试 icon 的 order 属性
+    // 注意元素出现在页面中的才会具有 CSS 属性
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    let button = new Constructor({
+      propsData: {
+        icon: 'settings',
+        iconPos: 'right',
+        loading: true
+      }
+    }).$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq("2")
+    button.$el.remove()
+    button.$destroy()
+  }
+  {
+    // 测试 click 事件
+    const Constructor = Vue.extend(Button)
+    let vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        iconPos: 'right',
+        loading: true
+      }
+    }).$mount()
+    let spy = chai.spy(() => { })
+    vm.$on('click', spy)
+    // 点击时，期望这个函数被执行
+    vm.$el.click()
+    expect(spy).to.have.been.called()
+    vm.$destroy()
+  }
+} catch (error) {
+  console.log(error)
+  window.errors = [error]
+} finally {
+  window.errors && window.errors.forEach(error => {
+    console.error(error.message)
   })
-  vm.$el.click()
-  vm.$destroy()
 }
