4519f03c17773937be826add59c48ba29eab6490
destinytaoer <1848765519@qq.com>
2019-06-14 16:42:38 +0800

    增加 karma 配置文件注释

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function (config) {
     basePath: '',
 
 
-    // frameworks to use
+    // 引入的框架，其会引入 mocha、sinon-chai，其中 sinon-chai 实际上会引入 sinon 和 chai 并将其合并，实际上我们使用的断言就是 sinon-chai 的 API，sinon 提供 spy，chai 提供 expect
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'sinon-chai'],
     client: {
@@ -15,7 +15,6 @@ module.exports = function (config) {
     },
 
 
-    // list of files / patterns to load in the browser
     // 需要测试的文件
     files: [
       // /**/*.test.js 表示不管是多少层级的 .test.js 文件
@@ -33,13 +32,13 @@ module.exports = function (config) {
     preprocessors: {},
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    // 测试结果的显示插件，
+    // 可选的值有: 'dots', 'progress'
+    // 更多的插件请查看: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
 
-    // web server port
+    // 开启的 web 服务端口
     port: 9876,
 
 
@@ -56,12 +55,12 @@ module.exports = function (config) {
     autoWatch: true,
 
 
-    // 需要开启的测试浏览器
+    // 需要开启的测试浏览器，ChromeHeadless 表示无头的 chrome，即隐藏的，如果使用的是 Chrome，那么在测试时会自动弹出一个浏览器，又自动关闭
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['ChromeHeadless'],
 
 
-    // Continuous Integration mode
+    // 持续集成模式
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
 
