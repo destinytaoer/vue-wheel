8b8d75aaea4a6fc56160a01b5cb8199e7ea8fdbb
destinytaoer <1848765519@qq.com>
2019-06-20 23:08:41 +0800

    [tabs] 完善 tabs 组件的校验

--- src/Tabs/Tabs.vue
+++ src/Tabs/Tabs.vue
@@ -36,14 +36,26 @@ export default {
     });
   },
   mounted() {
+    if (!this.$children.length) {
+      console.warn(
+        "d-tabs 组件的子组件应该拥有 d-tabs-head 和 d-tabs-body 组件"
+      );
+    }
     let itemNames = [];
     let contentNames = [];
     for (const children of this.$children) {
+      if (
+        children.$options.name !== "DTabsHead" &&
+        children.$options.name !== "DTabsBody"
+      )
+        console.warn(
+          "d-tabs 组件的子组件应该只是 d-tabs-head 和 d-tabs-body 组件"
+        );
       if (children.$options.name === "DTabsHead") {
         for (const item of children.$children) {
           if (item.$options.name === "DTabsItem") {
             if (itemNames.includes(item.name)) {
-              console.warn("d-tab-item 组件的 name 属性应该是唯一的！");
+              console.warn("d-tabs-item 组件的 name 属性应该是唯一的！");
             }
             if (item.disabled && item.name === this.selected) {
               console.warn("拥有 disabled: true 的 d-tab-items 不应该被选中！");
@@ -58,11 +70,11 @@ export default {
         for (const item of children.$children) {
           if (item.$options.name === "DTabsContent") {
             if (contentNames.includes(item.name)) {
-              console.warn("d-tab-content 组件的 name 属性应该是唯一的!");
+              console.warn("d-tabs-content 组件的 name 属性应该是唯一的!");
             }
             if (!itemNames.includes(item.name)) {
               console.warn(
-                "d-tab-content 组件的 name 属性应该有对应的 d-tab-item 组件！"
+                "d-tabs-content 组件的 name 属性应该有对应的 d-tab-item 组件！"
               );
             }
             contentNames.push(item.name);
