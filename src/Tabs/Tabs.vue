d3215ddf55d77fcf906d6dced56285763e55a5a9
destinytaoer <1848765519@qq.com>
2019-06-20 22:26:08 +0800

    [tab] 增加 name 属性的校验

--- src/Tabs/Tabs.vue
+++ src/Tabs/Tabs.vue
@@ -36,12 +36,36 @@ export default {
     });
   },
   mounted() {
+    let itemNames = [];
+    let contentNames = [];
     for (const children of this.$children) {
       if (children.$options.name === "DTabsHead") {
         for (const item of children.$children) {
           if (item.$options.name === "DTabsItem") {
+            if (itemNames.includes(item.name)) {
+              console.warn("d-tab-item 组件的 name 属性应该是唯一的！");
+            }
+            if (item.disabled && item.name === this.selected) {
+              console.warn("拥有 disabled: true 的 d-tab-items 不应该被选中！");
+            }
+            itemNames.push(item.name);
             item.name === this.selected &&
               this.eventBus.$emit("update:selected", this.selected, item);
+          }
+        }
+      }
+      if (children.$options.name === "DTabsBody") {
+        for (const item of children.$children) {
+          if (item.$options.name === "DTabsContent") {
+            if (contentNames.includes(item.name)) {
+              console.warn("d-tab-content 组件的 name 属性应该是唯一的!");
+            }
+            if (!itemNames.includes(item.name)) {
+              console.warn(
+                "d-tab-content 组件的 name 属性应该有对应的 d-tab-item 组件！"
+              );
+            }
+            contentNames.push(item.name);
           }
         }
       }
