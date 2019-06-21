d45138ed110d1c55f1389126c2dfe6e8c99a37a6
destinytaoer <1848765519@qq.com>
2019-06-21 17:22:49 +0800

    [poopover] 实现四个方位的 popover

--- src/Popover/Popover.vue
+++ src/Popover/Popover.vue
@@ -5,8 +5,9 @@
     ref="popover"
   >
     <div
-      class="content-wrapper"
+      class="popover-content-wrapper"
       ref="content"
+      :class="{[`position-${position}`]: true}"
       v-show="visible"
     >
       <slot name="content"></slot>
@@ -19,6 +20,15 @@
 <script>
 export default {
   name: "DPopover",
+  props: {
+    position: {
+      type: String,
+      default: "top",
+      validator(val) {
+        return ["top", "bottom", "left", "right"].includes(val);
+      }
+    }
+  },
   data() {
     return {
       visible: false
@@ -37,9 +47,29 @@ export default {
     },
     positionContent() {
       const { popover, content } = this.$refs;
-      let { top, left } = popover.getBoundingClientRect();
-      content.style.left = left + window.scrollX + "px";
-      content.style.top = top + window.scrollY + "px";
+      const position = this.position;
+      let { width, height, top, left } = popover.getBoundingClientRect();
+      let { height: contentHeight } = content.getBoundingClientRect();
+      let positions = {
+        top: {
+          left: left + window.scrollX,
+          top: top + window.scrollY
+        },
+        bottom: {
+          left: left + window.scrollX,
+          top: top + height + window.scrollY
+        },
+        left: {
+          left: left + window.scrollX,
+          top: top + window.scrollY + (height - contentHeight) / 2
+        },
+        right: {
+          left: left + window.scrollX + width,
+          top: top + window.scrollY + (height - contentHeight) / 2
+        }
+      };
+      content.style.left = positions[position].left + "px";
+      content.style.top = positions[position].top + "px";
     },
     hide() {
       this.visible = false;
@@ -69,14 +99,97 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+$border-color: #333;
+$border-radius: 4px;
 .popover {
   display: inline-block;
 }
-.content-wrapper {
+.popover-content-wrapper {
   position: absolute;
+  max-width: 20em;
+  word-break: break-all;
+  padding: 0.5em 1em;
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
   background: #fff;
-  border: 1px solid red;
-  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
-  transform: translateY(-100%);
+  // box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    display: block;
+    width: 0;
+    height: 0;
+    border: 10px solid transparent;
+  }
+  &.position-top {
+    margin-top: -10px;
+    transform: translateY(-100%);
+    &::before,
+    &::after {
+      left: 10px;
+      border-bottom: none;
+    }
+    &::before {
+      border-top-color: $border-color;
+      top: 100%;
+    }
+    &::after {
+      border-top-color: #fff;
+      top: calc(100% - 1px);
+    }
+  }
+  &.position-bottom {
+    margin-top: 10px;
+    &::before,
+    &:after {
+      left: 10px;
+      border-top: none;
+    }
+    &::before {
+      border-bottom-color: $border-color;
+      bottom: 100%;
+    }
+    &::after {
+      border-bottom-color: #fff;
+      bottom: calc(100% - 1px);
+    }
+  }
+  &.position-right {
+    margin-left: 10px;
+    &::before,
+    &::after {
+      border-left: none;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+    &::before {
+      border-right-color: $border-color;
+      right: 100%;
+    }
+    &::after {
+      border-right-color: #fff;
+      right: calc(100% - 1px);
+    }
+  }
+  &.position-left {
+    margin-left: -10px;
+    transform: translateX(-100%);
+    &::before,
+    &::after {
+      top: 50%;
+      border-right: none;
+      transform: translateY(-50%);
+    }
+    &::before {
+      border-left-color: $border-color;
+      left: 100%;
+    }
+    &::after {
+      border-left-color: #fff;
+      left: calc(100% - 1px);
+    }
+  }
 }
 </style>
